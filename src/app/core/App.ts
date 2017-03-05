c22e6007a3037abe120b05d63b23e5518589bc4f
Robert Hill <dr.pappa@gmail.com>
2017-03-05 19:13:01 +0000

    providing services to module

--- src/app/core/App.ts
+++
@@ -1,3 +0,0 @@
-import {TweetState} from "./state/TweetState";
-import {TweetStateUpdates} from "./state/TweetStateUpdates";
-import {TweetEvents} from "./events/TweetEvents";
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from '../components/app.component';
 
+import { TweetState } from "../core/state/TweetState";
+import { TweetStateUpdates } from "../core/state/TweetStateUpdates"
+import { TweetEvents } from "../core/events/TweetEvents";
+
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
+  providers:    [ TweetState, TweetStateUpdates, TweetEvents ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
