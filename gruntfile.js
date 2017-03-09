108d7bc15b9628b1048c4ed5458ab9374adbf630
Robert Hill <dr.pappa@gmail.com>
2017-03-09 19:46:51 +0000

    grunt

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: "./src/client",
+            src: ["**.html", "**.css"],
+            dest: "./dist/client"
+          }
+        ]
+      }
+    },
+    ts: {
+      app: {
+        files: [{
+          src: ["src/\*\*/\*.ts", "!src/.baseDir.ts"],
+          dest: "./dist"
+        }],
+        options: {
+          target: "es5",
+          module: "commonjs",
+          moduleResolution: "node",
+          sourceMap: true,
+          emitDecoratorMetadata: true,
+          experimentalDecorators: true,
+          lib: [ "es2015", "dom" ],
+          noImplicitAny: true,
+          suppressImplicitAnyIndexErrors: true
+        }
+      }
+    },
+    watch: {
+      ts: {
+        files: ["src/\*\*/\*.ts"],
+        tasks: ["ts"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-ts");
+
+  grunt.registerTask("default", [
+    "copy",
+    "ts"
+  ]);
+
+};
