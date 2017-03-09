1615104d49a87f4a5d1bc1ebed97964049c444d0
Robert Hill <dr.pappa@gmail.com>
2017-03-09 20:21:33 +0000

    server

---
+++ src/server/server.ts
@@ -0,0 +1,96 @@
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as path from "path";
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+export class Server {
+
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   * @return {ng.auto.IInjectorService} Returns the newly created injector for this app.
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    //create expressjs application
+    this.app = express();
+
+    //configure application
+    this.config();
+
+    //add routes
+    this.routes();
+
+    //add api
+    this.api();
+  }
+
+  /**
+   * Create REST API routes
+   *
+   * @class Server
+   * @method api
+   */
+  public api() {
+    //empty for now
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   */
+  public config() {
+    //add static paths
+    this.app.use(express.static(path.join(__dirname, "../client")));
+
+    //mount json form parser
+    this.app.use(bodyParser.json());
+
+    //mount query string parser
+    this.app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+
+    // catch 404 and forward to error handler
+    this.app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+        err.status = 404;
+        next(err);
+    });
+  }
+
+  /**
+   * Create and return Router.
+   *
+   * @class Server
+   * @method config
+   * @return void
+   */
+  private routes() {
+    let router: express.Router;
+    router = express.Router();
+
+    //use router middleware
+    this.app.use(router);
+  }
+
+}
