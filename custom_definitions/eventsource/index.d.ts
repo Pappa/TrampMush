d5cbbe1ae99acb17e39e58b4bb5b660e44607cbd
Robert Hill <dr.pappa@gmail.com>
2017-03-14 19:30:19 +0000

    Updated EventSource interface

--- custom_definitions/eventsource/index.d.ts
+++ custom_definitions/eventsource/index.d.ts
@@ -1,7 +1,7 @@
-declare enum ReadyState {
-	CONNECTING = 0,
-	OPEN,
-	CLOSED
+interface EventSourceMap {
+    "error": Event;
+    "message": MessageEvent;
+    "open": Event;
 }
 
 interface EventSourceConfig {
@@ -9,16 +9,24 @@ interface EventSourceConfig {
 }
 
 interface EventSource extends EventTarget {
-    readonly readyState: ReadyState;
+    readonly readyState: number;
     readonly url: string;
     readonly withCredentials: boolean;
-    onopen: () => any;
-    onmessage: (e: MessageEvent) => any;
-    onerror: ErrorEventHandler;
+	readonly CONNECTING: number;
+	readonly OPEN: number;
+	readonly CLOSED: number;
+    onopen: (this: EventSource, ev: Event) => any;
+    onmessage: (this: EventSource, ev: MessageEvent) => any;
+    onerror: (this: EventSource, ev: Event) => any;
     close: () => void;
+    addEventListener<K extends keyof EventSourceMap>(type: K, listener: (this: WebSocket, ev: EventSourceMap[K]) => any, useCapture?: boolean): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void;
 }
 
 declare var EventSource: {
     prototype: EventSource;
     new (url: string, configuration?: EventSourceConfig): EventSource;
+	readonly CONNECTING: number;
+	readonly OPEN: number;
+	readonly CLOSED: number;
 }
