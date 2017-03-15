5bbd1a128d9979912fd2d7ea2d41a9f5cbc4cb0f
Robert Hill <dr.pappa@gmail.com>
2017-03-15 19:00:29 +0000

    SSE middleware

---
+++ src/server/config/server.config.ts
@@ -0,0 +1,10 @@
+export var Config = {
+	server: {
+		"SSE_HEADERS": {
+	        "Content-Type": "text/event-stream",
+	        "Cache-Control": "no-cache",
+	        "Connection": "keep-alive",
+	        "transfer-encoding": ""
+		}
+	}
+}
