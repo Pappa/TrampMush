31ee8c37a4eb00370d2703ee9fda402a5d34ca98
Robert Hill <dr.pappa@gmail.com>
2017-03-21 17:39:44 +0000

    WIP

--- src/client/modules/core/events/TweetEvents.ts
+++ src/client/modules/core/events/TweetEvents.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/empty';
+import { Subscriber } from 'rxjs/Subscriber';
 import * as Models from '../models/Models';
 
 @Injectable()
@@ -30,6 +31,41 @@ export class TweetEvents {
         this.initGetSentiment();
         this.initGetTweetStream();
     }
+
+    /*private fromEventSource(url: string, openObserver: Function | null): Observable {
+      return new Observable(observer => {
+        const open = new Subscriber(openObserver);
+        const source = new EventSource(url);
+
+        const onOpen = event => {
+          open.next(event);
+          open.complete();
+        };
+
+        const onError = event => {
+          if (event.readyState === EventSource.CLOSED) {
+            observer.complete();
+          } else {
+            observer.error(event);
+          }
+        };
+
+        const onMessage = event => {
+          observer.next(event.data);
+        };
+
+        source.addEventListener('open', onOpen, false);
+        source.addEventListener('error', onError, false);
+        source.addEventListener('message', onMessage, false);
+
+        return () => {
+          source.removeEventListener('open', onOpen, false);
+          source.removeEventListener('error', onError, false);
+          source.removeEventListener('message', onMessage, false);
+          source.close();
+        };
+      });
+    }*/
 
     private initGetSentiment(): void {
         this.requests.getSentiment
